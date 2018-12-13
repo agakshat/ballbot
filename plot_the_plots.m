348a6c0326a6fb8ab535a2f2a31ea71b5501e791
agakshat <agarwalaks30@gmail.com>
2018-12-12 19:20:28 -0500

    better plots

--- plot_the_plots.m
+++ plot_the_plots.m
@@ -2,27 +2,34 @@ function plot_the_plots(times,THETA,phival,phidval,tau,rw)
 
 figure;
 subplot(3,2,1);
-plot(times,THETA(:,1)); grid on;
+plot(times,THETA(:,1),'linewidth',2); grid on;
+set(gca,'FontSize',10);
 xlabel('Time');
 ylabel('Angle \theta');
 subplot(3,2,2);
-plot(times,phival); grid on;
+plot(times,phival,'linewidth',2); grid on;
+set(gca,'FontSize',10);
+
 xlabel('Time');
 ylabel('Angle \phi');
 subplot(3,2,3);
-plot(times,THETA(:,2)); grid on;
+plot(times,THETA(:,2),'linewidth',2); grid on;set(gca,'FontSize',10);
+
 xlabel('Time');
-ylabel('Angular Velocity \theta^{.}');
+ylabel('\theta^{.}');
 subplot(3,2,4);
-plot(times,phidval); grid on;
+plot(times,phidval,'linewidth',2); grid on;set(gca,'FontSize',10);
+
 xlabel('Time');
-ylabel('Angular Velocity \phi^{.}');
+ylabel('\phi^{.}');
 subplot(3,2,5);
-plot(times,tau); grid on;
+plot(times,tau,'linewidth',2); grid on;set(gca,'FontSize',10);
+
 xlabel('Time');
-ylabel('Control Effort \tau');
+ylabel('Control \tau');
 subplot(3,2,6);
-plot(times,rw*(THETA(:,1)+phival)); grid on;
+plot(times,rw*(THETA(:,1)+phival),'linewidth',2); grid on;set(gca,'FontSize',10);
+
 xlabel('Time');
 ylabel('X');
 
